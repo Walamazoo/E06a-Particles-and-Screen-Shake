7be356b2fd4f7b56c6123ba16ac8cc6143641e1f
Walamazoo <59935897+Walamazoo@users.noreply.github.com>
2020-02-19 17:23:09 -0600

    Update World.gd

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -8,13 +8,9 @@ func _ready():
 func _physics_process(delta):
 	if Input.is_action_pressed("Left"):
 		$Camera.add_trauma(10)
-		$Camera._process(1)
 	if Input.is_action_pressed("Right"):
-		$Camera.add_trauma(10)
-		$Camera._process(1)
+		$Camera.add_trauma(20)
 	if Input.is_action_pressed("Up"):
-		$Camera.add_trauma(10)
-		$Camera._process(1)
+		$Camera.add_trauma(30)
 	if Input.is_action_pressed("Down"):
-		$Camera.add_trauma(10)
-		$Camera._process(1)
+		$Camera.add_trauma(40)
